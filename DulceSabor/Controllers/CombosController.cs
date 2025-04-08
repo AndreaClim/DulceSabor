884c12399ae57c9bba1654792e8bfeb8cb45ac4e
miche <miche@DESKTOP-EEADCHD>
2025-04-08 15:15:13 -0600

    añadiendo controladores

---
+++ DulceSabor/Controllers/CombosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DulceSabor.Controllers
+{
+    public class CombosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
