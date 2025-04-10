dfb8365506a8732a4d13ea0fab1da73b4cdc1c57
Amaya-2003 <caluchogamer@gmail.com>
2025-04-10 04:54:36 -0600

    MODULO COCINA, falta modificar la accion de detalles. Layout para modulos, tabla corregida y controlador de Cocina con sus vistas

--- DulceSabor/Controllers/CocinaController.cs
+++ DulceSabor/Controllers/CocinaController.cs
@@ -18,15 +18,45 @@ namespace DulceSabor.Controllers
                 .Where(p => p.estado == "Solicitado")
                 .ToList();
 
-            return View(pedidos);
-        }
+            var pedidosLinea = _context.pedido_linea
+                .Where(pl => pl.estado == "Pendiente")
+                .ToList();
 
+            // Pasamos ambos conjuntos de datos al ViewBag
+            ViewBag.Pedidos = pedidos;
+            ViewBag.PedidosLinea = pedidosLinea;
+
+            return View();
+        }
         public IActionResult Detalle(int id)
         {
-            var pedido = _context.pedidos.FirstOrDefault(p => p.id_pedido == id);
-            return View(pedido);
-        }
+            // Obtén los detalles del pedido con el id proporcionado
+            var detalles = _context.detalle_pedido
+                .Where(d => d.id_pedido == id)
+                .ToList();
 
+            // Obtener todos los platos para asociar con los detalles
+            var platos = _context.platos.ToList();
+
+            // Pasar tanto los detalles como los platos a la vista
+            ViewBag.Platos = platos;
+            return View(detalles);
+        }
+        public IActionResult DetalleL(int id)
+        {
+            var detalles = _context.detalle_linea
+                .Where(d => d.id_pedidoL == id)
+                .ToList();
+
+            var items = _context.item.ToList();
+            var platos = _context.platos.ToList();
+
+            ViewBag.Items = items;
+            ViewBag.Platos = platos;
+            ViewBag.IdPedidoLinea = id;
+
+            return View("DetalleL", detalles);
+        }
         [HttpPost]
         public IActionResult ActualizarEstado(int id)
         {
