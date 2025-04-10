f2f7fd80aeddbd8a46c91484eef45246d4db42ae
miche <miche@DESKTOP-EEADCHD>
2025-04-10 10:25:16 -0600

    botones añadidos para cambiar el estado del plato y pedido

--- DulceSabor/Controllers/CocinaController.cs
+++ DulceSabor/Controllers/CocinaController.cs
@@ -19,10 +19,10 @@ namespace DulceSabor.Controllers
                 .ToList();
 
             var pedidosLinea = _context.pedido_linea
-                .Where(pl => pl.estado == "Solicitado")
-                .ToList();
+            .Where(pl => pl.estado == "Solicitado") 
+            .ToList();
 
-            // Pasamos ambos conjuntos de datos al ViewBag
+            
             ViewBag.Pedidos = pedidos;
             ViewBag.PedidosLinea = pedidosLinea;
 
@@ -30,15 +30,15 @@ namespace DulceSabor.Controllers
         }
         public IActionResult Detalle(int id)
         {
-            // Obtén los detalles del pedido con el id proporcionado
+            
             var detalles = _context.detalle_pedido
                 .Where(d => d.id_pedido == id)
                 .ToList();
 
-            // Obtener todos los platos para asociar con los detalles
+            
             var platos = _context.platos.ToList();
 
-            // Pasar tanto los detalles como los platos a la vista
+         
             ViewBag.Platos = platos;
             return View(detalles);
         }
@@ -55,8 +55,10 @@ namespace DulceSabor.Controllers
             ViewBag.Platos = platos;
             ViewBag.IdPedidoLinea = id;
 
-            return View("DetalleL", detalles);
+
+            return View(detalles);
         }
+
         [HttpPost]
         public IActionResult ActualizarEstado(int id)
         {
@@ -69,6 +71,116 @@ namespace DulceSabor.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpPost]
+        public IActionResult CambiarEstadoDetallePedido(int id)
+        {
+            var detalle = _context.detalle_pedido.FirstOrDefault(d => d.id_detalle == id);
+            if (detalle != null)
+            {
+               
+                detalle.estado = detalle.estado == "En proceso" ? "Finalizado" : "En proceso";
+                _context.SaveChanges();
+
+                var todosEnProceso = _context.detalle_pedido
+                    .Where(d => d.id_pedido == detalle.id_pedido)
+                    .All(d => d.estado == "En proceso");
+
+                
+                var todosFinalizados = _context.detalle_pedido
+                    .Where(d => d.id_pedido == detalle.id_pedido)
+                    .All(d => d.estado == "Finalizado");
+
+                
+                if (todosEnProceso)
+                {
+                    var pedido = _context.pedidos.FirstOrDefault(p => p.id_pedido == detalle.id_pedido);
+                    if (pedido != null)
+                    {
+                        pedido.estado = "En proceso";
+                        _context.SaveChanges();
+                    }
+                }
+                else if (todosFinalizados)
+                {
+                    var pedido = _context.pedidos.FirstOrDefault(p => p.id_pedido == detalle.id_pedido);
+                    if (pedido != null)
+                    {
+                        pedido.estado = "Finalizado";
+                        _context.SaveChanges();
+                    }
+                }
+                else
+                {
+                    var pedido = _context.pedidos.FirstOrDefault(p => p.id_pedido == detalle.id_pedido);
+                    if (pedido != null)
+                    {
+                        pedido.estado = "Solicitado";
+                        _context.SaveChanges();
+                    }
+                }
+            }
+
+            return RedirectToAction("Detalle", new { id = detalle.id_pedido });
+        }
+
+
+
+        [HttpPost]
+        public IActionResult CambiarEstadoDetalleLinea(int id)
+        {
+            var detalle = _context.detalle_linea.FirstOrDefault(d => d.id_dpedido == id);
+            if (detalle != null)
+            {
+              
+                detalle.estado = detalle.estado == "En proceso" ? "Finalizado" : "En proceso";
+                _context.SaveChanges();
+
+                
+                var todosEnProceso = _context.detalle_linea
+                    .Where(d => d.id_pedidoL == detalle.id_pedidoL)
+                    .All(d => d.estado == "En proceso");
+
+                var todosFinalizados = _context.detalle_linea
+                    .Where(d => d.id_pedidoL == detalle.id_pedidoL)
+                    .All(d => d.estado == "Finalizado");
+
+                if (todosEnProceso)
+                {
+                    var pedido = _context.pedido_linea.FirstOrDefault(p => p.id_pedidoL == detalle.id_pedidoL);
+                    if (pedido != null)
+                    {
+                        pedido.estado = "En proceso"; 
+                        _context.SaveChanges();
+                    }
+                }
+               
+                else if (todosFinalizados)
+                {
+                    var pedido = _context.pedido_linea.FirstOrDefault(p => p.id_pedidoL == detalle.id_pedidoL);
+                    if (pedido != null)
+                    {
+                        pedido.estado = "Finalizado"; 
+                        _context.SaveChanges();
+                    }
+                }
+                else
+                {
+                   
+                    var pedido = _context.pedido_linea.FirstOrDefault(p => p.id_pedidoL == detalle.id_pedidoL);
+                    if (pedido != null)
+                    {
+                        pedido.estado = "Solicitado"; 
+                        _context.SaveChanges();
+                    }
+                }
+            }
+
+            return RedirectToAction("DetalleL", new { id = detalle.id_pedidoL });
+        }
+
+
+
     }
 
 }
--- DulceSabor/Models/detalle_pedido.cs
+++ DulceSabor/Models/detalle_pedido.cs
@@ -8,6 +8,7 @@ namespace DulceSabor.Models
         public int id_pedido { get; set; }
         public int id_plato { get; set; }
         public string? comentarios { get; set; }
-       
+        public string estado { get; set; }
+
     }
 }
